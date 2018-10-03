2d059ee883d1b222dabad8432d337f2e3ac81206
Marc DeHaan <barcikm@comcast.net>
2018-10-02 18:17:21 -0700

    Update PokerDeck class tests
    Implement tests on every function

--- CardGame/CardGameTests/PokerDeckTests.cs
+++ CardGame/CardGameTests/PokerDeckTests.cs
@@ -21,11 +21,70 @@ namespace CardGameTests
         }
 
         [Fact]
+        public void TestGetCard()
+        {
+            PokerDeck deck = new PokerDeck();
+            deck.getCard();
+            Assert.Equal(51, deck.getCount());
+        }
+
+        [Fact]
+        public void TestDisplayAllCardsDoesntChangeCount()
+        {
+            PokerDeck deck = new PokerDeck();
+            deck.displayCardsInDeck();
+            Assert.Equal(52, deck.getCount());
+        }
+
+        [Fact]
         public void TestDisplayCardsDoesntChangeCount()
         {
+            //Overloaded function test
             PokerDeck deck = new PokerDeck();
             deck.displayCardsInDeck(4);
             Assert.Equal(52, deck.getCount());
         }
+
+        [Fact]
+        public void TestShuffle() {
+            /*can't test object directly as there is a
+            * chance it might be the same after a sort.
+            * So test that the method runs
+            */
+            PokerDeck deck = new PokerDeck();
+            {
+                try
+                {
+                    deck.shuffle();
+                    Assert.True(true);
+                }
+                catch
+                {
+                    Assert.True(false);
+                }
+            }
+        }
+
+        [Fact]
+        public void TestCountCardsLeftInDeck()
+        {
+            /*can't test object directly as there is a
+            * chance it might be the same after a sort.
+            * So test that the method runs
+            */
+            PokerDeck deck = new PokerDeck();
+            {
+                try
+                {
+                    deck.countCardsLeftInDeck();
+                    Assert.True(true);
+                }
+                catch
+                {
+                    Assert.True(false);
+                }
+            }
+        }
+
     }
 }
