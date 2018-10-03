befa1af59b791f2a0a2503b44bddc2e8f0983adb
Marc DeHaan <barcikm@comcast.net>
2018-10-02 18:45:00 -0700

    Updated unit testing for the User Interface

---
+++ CardGame/CardGameTests/UserInterfaceTests.cs
@@ -0,0 +1,42 @@
+﻿using CardGame;
+using Xunit;
+
+namespace CardGameTests
+{
+    public class UserInterfaceTests
+    {
+        [Fact]
+        public void TestMenuFunction()
+        {
+            UserInterface app = new UserInterface();
+            {
+                try
+                {
+                    app.startMenu();
+                    Assert.True(true);
+                }
+                catch
+                {
+                    Assert.True(false);
+                }
+            }
+        }
+
+        [Fact]
+        public void TestContinuePromptFunction()
+        {
+            UserInterface app = new UserInterface();
+            {
+                try
+                {
+                    app.continuePrompt();
+                    Assert.True(true);
+                }
+                catch
+                {
+                    Assert.True(false);
+                }
+            }
+        }
+    }
+}
