45e2ec3cd8f2053d1019dde6ea9bf83184a31398
Marc DeHaan <barcikm@comcast.net>
2018-10-02 18:27:10 -0700

    Update Card Class Tests

---
+++ CardGame/CardGameTests/CardTests.cs
@@ -0,0 +1,31 @@
+﻿using CardGame;
+using Xunit;
+
+namespace CardGameTests
+{
+    public class CardTests
+    {
+        [Fact]
+        public void TestCardSuit()
+        {
+            Card card = new Card(CardSuits.Spades, 14);
+            Assert.Equal(CardSuits.Spades, card.Suit);
+        }
+
+        [Fact]
+        public void TestCardValue()
+        {
+            Card card = new Card(CardSuits.Spades, 14);
+            Assert.Equal(14, card.Value);
+        }
+
+        [Fact]
+        public void TestCardToString()
+        {
+            Card card = new Card(CardSuits.Spades, 14);
+            Assert.Equal("Ace of Spades", card.ToString());
+        }
+
+    }
+}
+
